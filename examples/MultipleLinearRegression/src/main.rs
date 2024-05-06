b5efa187c5c548b2a10c4c0e9d829906fa775e6d
Lyn-liyuan <lyn@intellicloud.ai>
2024-05-06 09:18:12 +0000

    add coments for main.rs

--- examples/MultipleLinearRegression/src/main.rs
+++ examples/MultipleLinearRegression/src/main.rs
@@ -1,54 +1,58 @@
 use ndarray::Array2;
-use ndarray_cuda_matmul::{run, run2, MatDot, MatInv, MatT, ToHost};
+use ndarray_cuda_matmul::{MatDot, MatInv, MatT, ToHost};
 use ndarray_linalg::Inverse;
 use rand_distr::{Distribution, Normal, Uniform};
 use std::time::Instant;
 
 fn gen_data() -> (Array2<f32>, Array2<f32>) {
-    // 设置线性模型的参数
+    // Set parameters for the linear model
     let w = [
         3.0f32, 2.0f32, 1.5f32, 4.2f32, 3.0f32, 6.2f32, 1.7f32, 1.2f32, 0.3f32,
-    ]; // 线性系数
-    let b = 0.7f32; // 截距项
-    let n = 100000000; // 数据点数量
+    ]; // Linear coefficients
+    let b = 0.7f32; // Intercept term
+    let n = 100000000; // Number of data points
 
-    // 创建随机数生成器
+    // Create random number generator
     let mut rng = rand::thread_rng();
-    let uniform_dist = Uniform::new(0.0, 10.0); // 输入值的范围
-    let normal_dist = Normal::new(0.0, 1.0).unwrap(); // 噪音的分布
+    let uniform_dist = Uniform::new(0.0, 10.0); // Range for input values
+    let normal_dist = Normal::new(0.0, 1.0).unwrap(); // Distribution for noise
 
-    // 初始化矩阵以存储输入数据和目标数据
+    // Initialize matrices to store input data and target data
     let mut data: Array2<f32> = Array2::zeros((n, 10));
     let mut y: Array2<f32> = Array2::zeros((n, 1));
 
     for i in 0..n {
         let noise = normal_dist.sample(&mut rng);
 
-        //截距项为1，接着是x值，最后是y值
-        data[[i, 0]] = 1.0f32; // 添加截距项
+        // The first entry is for the intercept, followed by x values, and finally the y value
+        data[[i, 0]] = 1.0f32; // Add the intercept term
         let mut y_hat = 0.0f32;
         for j in 1..10 {
+            // Generate random x values
             data[[i, j]] = uniform_dist.sample(&mut rng);
+            // Calculate the linear combination of x values and weights
             y_hat += w[j - 1] * data[[i, j]];
         }
+        // Assign the calculated y value with added noise
         y[[i, 0]] = y_hat + b + noise;
     }
     return (data, y);
 }
 
+
 fn do_ndarray_linalg(x: &Array2<f32>, y: &Array2<f32>) {
     let xt = x.t();
 
-    // 计算 X^T * X
+    // calculate X^T * X
     let xtx = xt.dot(x);
 
-    // 计算 (X^T * X)^(-1)
+    // calculate (X^T * X)^(-1)
     let xtx_inv = xtx.inv().unwrap();
 
-    // 计算 (X^T * X)^(-1) * X^T
+    // calculate (X^T * X)^(-1) * X^T
     let xtx_inv_xt = xtx_inv.dot(&xt);
 
-    // 计算 ((X^T * X)^(-1) * X^T) * y，得到系数 b
+    // calculate ((X^T * X)^(-1) * X^T) * y，to get coefficient b
     let beta = xtx_inv_xt.dot(y);
 
     println!("{:?}", beta);
@@ -56,6 +60,7 @@ fn do_ndarray_linalg(x: &Array2<f32>, y: &Array2<f32>) {
 
 fn do_cuda(x: &Array2<f32>, y: &Array2<f32>) {
     let out = run!(x,y => {
+        // (X^TX)^{-1}X^Ty
         let x_t = x.t();
         x_t.dot(x).inv().dot(&x_t).dot(y)
     })
